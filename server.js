d16fe7177b56d17cecc3d7949f2d341cf12abb5f
lbzinuse75 <130871536+lbzinuse75@users.noreply.github.com>
2025-09-24 13:57:01 -0600

    6 complete

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+//Index route
+app.get("/", function(req, res){res.render("index", {title: "Home"})})
 
 /* ***********************
  * Local Server Information
